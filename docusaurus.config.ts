ff98795a523270dc56c65d7fdcee93dba9ada5a2
Seven Thomas <sevenmyles33@gmail.com>
2026-04-07 11:50:43 -0400

    Add GitHub Actions workflow for Docusaurus deployment and update site configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://intealth-onboarding.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Onboarding/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Intealth-Onboarding', // Usually your GitHub org/user name.
+  projectName: 'Onboarding', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
